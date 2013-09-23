ac09fbd8c950a37c37c25b1c2cd9abc5050bcf9f
Richard Roberts <rykardo.r@gmail.com>
2013-09-23 22:32:02 +1200

    this is more complicated than I can compreheend

--- src/Generator.hs
+++ src/Generator.hs
@@ -14,10 +14,11 @@ import LocationSelection
 --------------------------------------------------------------------------------
 -- |
 
+type InstructSet = (Int, PolyType, Point, Point, Point, [Int], [Double])
 
 data BranchStump = BranchStump
     { 
-      expansion :: [Int],
+      expansion :: InstructSet,
       childs :: [BranchStump]
     }
 
@@ -40,16 +41,31 @@ createBranch nPolyType loc gScale pScale up parent = Branch newNode parent []
 
 --------------------------------------------------------------------------------
 -- | Creates a branch at each of the given branchs locations
-expand :: Int -> PolyType -> Point -> Point -> Point -> [Int] -> [Double] -> Branch -> [Branch]
-expand selectID nPolyType gS pS up ints doubs parent = map (\loc -> createBranch nPolyType loc gS pS up parent) locs
+expand :: InstructSet -> Branch -> [Branch]
+expand (selectID, nPolyType, gS, pS, up, ints, doubs) parent = map (\loc -> createBranch nPolyType loc gS pS up parent) locs
   where locs = transformLocs (selectLocations selectID (node parent) ints doubs) (pScale (node parent))
 
 --------------------------------------------------------------------------------
 -- | Prints out a triple.
-recur :: [[(Int, PolyType, Point, Point, Point, [Int], [Double])]] -> Branch -> [Branch]
-recur [] _ = []
-recur (i:is) b = b : (concat $ map (\br -> recur is br) branches)
-  where branches = concat $ map (\(selId, pType, gScale, pScale, up, ints, doubs) -> expand selId pType gScale pScale up ints doubs b) i
+--recur :: BranchStump -> Branch -> [Branch]
+--recur [] _ = []
+--recur (i:is) b = b : (concat $ map (\br -> recur is br) branches)
+--  where branches = concat $ map (\(selId, pType, gScale, pScale, up, ints, doubs) -> expand selId pType gScale pScale up ints doubs b) i
 
 --------------------------------------------------------------------------------
 --------------------------------------------------------------------------------
+
+--recur :: [BranchStump] -> Branch -> [Branch]
+--recur (BranchStump _ []) b = [b]
+--recur bStump bParent = bParent : (concat $ map (\(currentChild, createdBranch) -> recur currentChild createdBranch) combs)
+--  where combs = [(c, b) | c <- (childs bStump), b <- createdBranches]
+--        createdBranches = expand (expansion bStump) bParent
+
+recur :: [BranchStump] -> Branch -> [Branch]
+recur [] b = [b]
+recur bStumps bParent = bParent : (concat $ map (\bs -> results bs) bStumps)
+  where results bStump = concat $ map (\aBranch -> recur (childs bStump) aBranch) (expand (expansion bStump) bParent)
+
+
+--spawn children
+--each child recur
--- src/Main.hs
+++ src/Main.hs
@@ -30,24 +30,20 @@ showTriple p = putStrLn $ printf "x:%2.2f, y:%2.2f, z:%2.2f" (px p) (py p) (pz p
 main :: IO ()
 main = do
     -- Make the first branch of the pattern.
-    let org = Polygon PolyCylinder (tMtx (0, 0, 0)) (sMtx (1, 2, 1))
+    let org = Polygon PolyCylinder (tMtx (0, 0, 0)) (sMtx (1, 1, 1))
     let b1 = Branch org Empty []
 
     -- Give the recursion instructions
-    let struct = [
-                    [
-                       (1, PolyCube,   (0.3, 0.3, 0.3), (0.4, 0.05, 1.2), (0, 0, 1), [16], [0.5]),
-                       (1, PolyCube,   (0.3, 0.3, 0.3), (0.1, 1.2, 0.05), (0, 0, 1), [16], [0.5]),
-                       (1, PolyCube,   (0.3, 0.3, 0.3), (0.1, 1.8, 0.15), (0, 0, 1), [8], [-0.5]),
-                       (1, PolyCube,   (0.3, 0.3, 0.3), (0.1, 0.15, 1.8), (0, 0, 1), [8], [-0.3]),
-                       (1, PolyCube,   (0.3, 0.3, 0.3), (0.1, 0.15, 1.8), (0, 0, 1), [8], [-0.7])],
-                    [
-                       (0, PolyCube,   (0.3, 0.3, 0.3), (0.4, 0.05, 1.2), (0, 0, 1), [1,0,0], [])],
-                    [
-                       (0, PolyCube,   (0.3, 0.3, 0.3), (1.0, 1.0, 1.0), (0, 1, 0), [0,0,0], [0.0])]]
+    --let fake = BranchStump (0, PolyCube, (0.3, 0.3, 0.3), (0.4, 0.05, 1.2), (0, 0, 1), [1,1,1], [0.5]) []
+
+    let cS2 = BranchStump (0, PolyCube, (0.3, 0.3, 0.3), (1,1,1), (0, 1, 0), [1, 1, 1], []) []
+    let cS1 = BranchStump (1, PolyCube, (0.3, 0.3, 0.3), (1,1,1), (1, 0, 0), [8], [0.99]) []
+
+    let bS1 = BranchStump (0, PolyCylinder, (0.3, 0.3, 0.3), (1,1,1), (0, 1, 0), [0, 1, 0], []) [cS2, cS1]
+    let bS2 = BranchStump (0, PolyCylinder, (0.3, 0.3, 0.3), (1,1,1), (0, 1, 0), [0, 0, 1], []) [cS2]
 
     -- Recusrively expand it.
-    let pSet = map (\b -> (node b)) (recur struct b1)
+    let pSet = map (\b -> (node b)) (recur [bS1, bS2] b1)
 
     -- Write the python script
     let title = "basic"
