c2311a5a43b77b5dc7c7b229ba428eb5febd6be8
Richard Robers <rykardo.r@gmail.com>
2013-10-28 16:03:25 +1300

    Adding in axis angle

--- src/Main.hs
+++ src/Main.hs
@@ -12,34 +12,73 @@ module Main where
 -- Standard
 import Control.Applicative
 import Data.Tree
+import Data.Matrix
 
 -- Source
 import DetailGen
 import PointSelect
+import Transform
 
--- Get the current label.
+
+
+------------------------------------------------------------------------------
+getPoints :: Detail -> [Point]
+getPoints (Detail p _ _) = p
+
+
+------------------------------------------------------------------------------
+getShape :: Detail -> Shape
+getShape (Detail _ s _) = s
+
+
+------------------------------------------------------------------------------
+getScale :: Detail -> Double
+getScale (Detail _ _ s) = s
+
+------------------------------------------------------------------------------
 getLabel :: Tree Detail -> Detail
 getLabel = rootLabel
 
+
+------------------------------------------------------------------------------
 getSub :: Tree Detail -> Forest Detail
 getSub = subForest
 
-unwrapTree :: Int -> Tree Detail -> IO ()
+
+------------------------------------------------------------------------------
+expand :: Point -> Matrix Double -> Int -> Forest Detail -> IO ()
+expand p pMtx l sub = do
+	let newM = dotM pMtx
+	mapM_ (unwrapTree (l + 1)) sub
+	return ()
+
+
+------------------------------------------------------------------------------
+unwrapTree :: Matrix Double -> Int -> Tree Detail -> IO ()
 unwrapTree l x = do
     let cLabel = getLabel x
     let cSub =  getSub x
 
     putStrLn $ replicate (l * 2) ' ' ++ show cLabel
 
-    mapM_ (unwrapTree (l+1)) cSub
+    
     return ()
 
 ------------------------------------------------------------------------------
 main :: IO ()
 main = do
-    let segment = do { detail Cylinder (CubeFaces xAxis) 0.5 ; branch [pure 0.25, pure 0.5, pure 0.7]}
-    let cubeSegment = do { height <- segment ; detail Cube (CylinderLoop 4 height) 0.2 ; branch [pure 0.2]}
-    let cubeSegment2 = do { height2 <- cubeSegment ; detail Cube (CylinderLoop 4 height2) 0.2 }
+	sphereThenCylinder = do
+    	detail Sphere (CubeFaces yAxis) 0.3           ;
+    	detail Cylinder (SphereLoop 16 0) 0.3         }
+	cubeThenCylinder = do
+		detail Cube (CubeFaces xAxis) 0.1             ;
+		detail Cylinder (CubeFaces zAxis) 0.9         }
+	applyDetail = do {
+		detail Cube (CylinderLoop 8 0.5) 0.2          ;
+		branch [sphereThenCylinder, cubeThenCylinder] ;
+		detail Sphere (CylinderLoop 4 0.2) 0.4        }
+
+
     unwrapTree 0 $ head (runDetailGen cubeSegment2)
 
 ------------------------------------------------------------------------------
